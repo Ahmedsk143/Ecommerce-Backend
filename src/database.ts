f7a8bd4d3c9813c300190d1cc70e0cdc5a146ffd
Ahmedsk143 <ahmedsayedkhalil143@gmail.com>
2022-12-21 19:50:30 +0200

    ..

--- src/database.ts
+++ src/database.ts
@@ -2,35 +2,12 @@ import { Pool } from 'pg';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-
-const { ENV } = process.env;
-let DBConnection: Pool;
-
-if (ENV === 'dev') {
-    DBConnection = new Pool({
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME_DEV,
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT as unknown as number,
-    });
-} else if (ENV === 'test') {
-    DBConnection = new Pool({
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME_TEST,
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT as unknown as number,
-    });
-} else {
-    // Production
-    DBConnection = new Pool({
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME_TEST,
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT as unknown as number,
-    });
-}
+const DBConnection = new Pool({
+    // user: process.env.DB_USERNAME,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
+    connectionString: process.env.DATABASE_URL,
+});
 
 export default DBConnection;
